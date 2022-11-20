79f98822412c3795bb351d29c76ed95532806d2a
paralak <paralakmc@gmail.com>
2022-11-20 22:31:57 +0300

    create class Arrow

---
+++ paralak/arrow.js
@@ -0,0 +1,25 @@
+class Arrow {
+    startElement = null
+    endElement = null
+
+    constructor (args) {
+        this.startElement = args.start;
+        this.ctx = args.ctx;
+    }
+
+    drow (field) {
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = "rgb(0, 0, 0)";
+        this.ctx.moveTo(
+            (this.startElement.x + 1)*field.pxW - field.startX,
+            (this.startElement.y + 1)*field.pxH - field.startY
+        );
+        this.ctx.lineTo(
+            (this.endElement.x + 1)*field.pxW - field.startX,
+            (this.endElement.y + 1)*field.pxH - field.startY
+        );
+        this.ctx.stroke();
+        this.ctx.closePath();
+    }
+
+}
